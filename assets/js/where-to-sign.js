8d7186bc4e11de884a3fd23fe4a882112013888c
imtaiwanese18741130 <imtaiwanese18741130>
2025-01-23 08:39:21 +0000

    update where to sign notify

--- assets/js/where-to-sign.js
+++ assets/js/where-to-sign.js
@@ -7,6 +7,7 @@ let flashInterval = null;
 function drawBorder(rect, isVisible) {
   const overlayContainer = document.getElementById("where-to-sign");
   const borderDiv = document.createElement("div");
+
   borderDiv.style.position = "absolute";
   borderDiv.style.border = isVisible ? "4px dashed #ff073a" : "none";
   borderDiv.style.pointerEvents = "none";
@@ -14,6 +15,22 @@ function drawBorder(rect, isVisible) {
   borderDiv.style.left = `${rect.left}px`;
   borderDiv.style.width = `${rect.width}px`;
   borderDiv.style.height = `${rect.height}px`;
+
+  if (isVisible) {
+    const textDiv = document.createElement("div");
+    textDiv.textContent = "在這簽名";
+    textDiv.style.position = "absolute";
+    textDiv.style.top = "50%";
+    textDiv.style.left = "50%";
+    textDiv.style.transform = "translate(-50%, -50%)";
+    textDiv.style.color = "#ff073a";
+    textDiv.style.fontSize = "18px";
+    textDiv.style.fontWeight = "bold";
+    textDiv.style.pointerEvents = "none";
+
+    borderDiv.appendChild(textDiv);
+  }
+
   overlayContainer.appendChild(borderDiv);
 }
 
