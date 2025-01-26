6e730c8d1437e86ee5beb1c10f1714ed65645ef3
imtaiwanese18741130 <imtaiwanese18741130>
2025-01-26 22:43:38 +0000

    1st round pdf check

--- assets/js/common.js
+++ assets/js/common.js
@@ -7,34 +7,43 @@ async function downloadPDF(containerSelector, filename, redirectURL) {
 		return;
 	}
 
-	const whereToSigns = document.querySelectorAll('.whereToSign');
-	whereToSigns.forEach((element) => {
-		if (getComputedStyle(element).display !== 'none') {
-			element.style.display = 'none';
+	const mask = document.querySelector('.mask');
+	mask.classList.add('active');
+
+	try {
+		const whereToSigns = document.querySelectorAll('.whereToSign');
+		whereToSigns.forEach((element) => {
+			if (getComputedStyle(element).display !== 'none') {
+				element.style.display = 'none';
+			}
+		});
+		await new Promise((resolve) => setTimeout(resolve, 100));
+
+		const isLandscape = containerSelector === '.a4-landscape';
+		const orientation = isLandscape ? 'l' : 'p';
+
+		const pdf = new jsPDF(orientation, 'mm', 'a4');
+		const pdfWidth = pdf.internal.pageSize.getWidth();
+
+		for (const [index, container] of containers.entries()) {
+			const canvas = await html2canvas(container, { scale: 2 });
+			const imgData = canvas.toDataURL('image/png');
+			const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+
+			if (index > 0) {
+				pdf.addPage();
+			}
+
+			pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
 		}
-	});
-	await new Promise((resolve) => setTimeout(resolve, 100));
 
-	const isLandscape = containerSelector === '.a4-landscape';
-	const orientation = isLandscape ? 'l' : 'p';
-
-	const pdf = new jsPDF(orientation, 'mm', 'a4');
-	const pdfWidth = pdf.internal.pageSize.getWidth();
-
-	for (const [index, container] of containers.entries()) {
-		const canvas = await html2canvas(container, { scale: 2 });
-		const imgData = canvas.toDataURL('image/png');
-		const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
-
-		if (index > 0) {
-			pdf.addPage();
-		}
-
-		pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+		pdf.save(`${filename}.pdf`);
+		window.location.href = redirectURL;
+	} catch (error) {
+		console.error('Error generating PDF:', error);
+	} finally {
+		mask.classList.remove('active');
 	}
-
-	pdf.save(filename + '.pdf');
-	window.location.href = redirectURL;
 }
 async function copyLink(url) {
 	navigator.clipboard.writeText(url)
