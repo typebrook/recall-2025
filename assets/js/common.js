673b242fdd4644ccecddd7811611430dec469496
imtaiwanese18741130 <imtaiwanese18741130>
2025-01-26 23:25:54 +0000

    1st all zone version

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,49 +1,44 @@
 async function downloadPDF(containerSelector, filename, redirectURL) {
-	const { jsPDF } = window.jspdf;
-	const containers = document.querySelectorAll(containerSelector);
+  const { jsPDF } = window.jspdf;
+  const containers = document.querySelectorAll(containerSelector);
+  if (containers.length === 0) {
+    console.error('No elements found with the given class name.');
+    return;
+  }
 
-	if (containers.length === 0) {
-		console.error('No elements found with the given class name.');
-		return;
-	}
+  const mask = document.querySelector('.mask');
+  mask.classList.add('active');
+  try {
+    const whereToSigns = document.querySelectorAll('.whereToSign');
+    whereToSigns.forEach((element) => {
+      if (getComputedStyle(element).display !== 'none') {
+        element.style.display = 'none';
+      }
+    });
+    await new Promise((resolve) => setTimeout(resolve, 100));
 
-	const mask = document.querySelector('.mask');
-	mask.classList.add('active');
+    const isLandscape = containerSelector === '.a4-landscape';
+    const orientation = isLandscape ? 'l' : 'p';
+    const pdf = new jsPDF(orientation, 'mm', 'a4');
+    const pdfWidth = pdf.internal.pageSize.getWidth();
 
-	try {
-		const whereToSigns = document.querySelectorAll('.whereToSign');
-		whereToSigns.forEach((element) => {
-			if (getComputedStyle(element).display !== 'none') {
-				element.style.display = 'none';
-			}
-		});
-		await new Promise((resolve) => setTimeout(resolve, 100));
+    for (const [index, container] of containers.entries()) {
+      const canvas = await html2canvas(container, { scale: 2 });
+      const imgData = canvas.toDataURL('image/png');
+      const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+      if (index > 0) {
+        pdf.addPage();
+      }
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+    }
 
-		const isLandscape = containerSelector === '.a4-landscape';
-		const orientation = isLandscape ? 'l' : 'p';
-
-		const pdf = new jsPDF(orientation, 'mm', 'a4');
-		const pdfWidth = pdf.internal.pageSize.getWidth();
-
-		for (const [index, container] of containers.entries()) {
-			const canvas = await html2canvas(container, { scale: 2 });
-			const imgData = canvas.toDataURL('image/png');
-			const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
-
-			if (index > 0) {
-				pdf.addPage();
-			}
-
-			pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-		}
-
-		pdf.save(`${filename}.pdf`);
-		window.location.href = redirectURL;
-	} catch (error) {
-		console.error('Error generating PDF:', error);
-	} finally {
-		mask.classList.remove('active');
-	}
+    pdf.save(`${filename}.pdf`);
+    window.location.href = redirectURL;
+  } catch (error) {
+    console.error('Error generating PDF:', error);
+  } finally {
+    mask.classList.remove('active');
+  }
 }
 async function copyLink(url) {
 	navigator.clipboard.writeText(url)
