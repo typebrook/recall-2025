e37214ffb0961f4f25aa1d5fb4d51627787153f0
imtaiwanese18741130 <imtaiwanese18741130@gmail.com>
2025-02-26 05:04:00 +0000

    update thank-you page

--- controllers.go
+++ controllers.go
@@ -392,7 +392,7 @@ func (ctrl Controller) Sitemap() gin.HandlerFunc {
 
 		for _, l := range ctrl.RecallLegislators {
 			legislatorURL := ctrl.AppBaseURL.JoinPath("stages", strconv.FormatUint(l.RecallStage, 10), l.PoliticianName)
-			if l.FormDeployed && l.RecallStatus == "ONGOING" {
+			if l.RecallStatus == "ONGOING" {
 				urls = append(urls, &SitemapURL{legislatorURL.String(), "2025-03-03", "weekly", "0.9"})
 				urls = append(urls, &SitemapURL{legislatorURL.JoinPath("thank-you").String(), "2025-03-03", "weekly", "0.8"})
 			}
